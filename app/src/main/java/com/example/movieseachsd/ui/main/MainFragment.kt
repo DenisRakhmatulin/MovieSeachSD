c2b909a08785007fbbce7b464feb16bcb6c54028
DenisRakhmatulin <sardavis82@gmail.com>
2022-03-31 19:05:15 +1200

    Add dependencies injection - Koin Koin module build viewBinding

---
+++ app/src/main/java/com/example/movieseachsd/ui/main/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.movieseachsd.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieseachsd.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class MainFragment : Fragment() {
+
+
+
+    private val viewModel: MainViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+}
