9182018cb52986777d8ddb9d0126eb9485e0b049
DenisRakhmatulin <sardavis82@gmail.com>
2022-04-03 13:13:14 +1200

    Checkpoint 2 MVVM logic hardcoded

--- app/src/main/java/com/example/movieseachsd/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/movieseachsd/ui/main/MainFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.movieseachsd.R
 import com.example.movieseachsd.databinding.MainFragmentBinding
+import com.example.movieseachsd.model.AppState
+import com.example.movieseachsd.model.entites.Details
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -29,6 +33,40 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<AppState> { renderData(it) }
+        viewModel.liveData.observe(viewLifecycleOwner, observer)
+        viewModel.getDetails()
+    }
+
+    private fun renderData(appState: AppState) = with(binding) {
+        when (appState) {
+            is AppState.Success -> {
+                val detailsData = appState.detailsData
+                progressBar.visibility = View.GONE
+                mainFragmentRecyclerView.visibility = View.VISIBLE
+                setData(detailsData)
+            }
+            is AppState.Loading -> {
+                progressBar.visibility = View.VISIBLE
+                mainFragmentRecyclerView.visibility = View.INVISIBLE
+            }
+            is AppState.Error -> {
+                progressBar.visibility = View.GONE
+                mainFragmentRecyclerView.visibility = View.INVISIBLE
+                Snackbar
+                    .make(mainView, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") {viewModel.getDetails()}
+                    .show()
+            }
+
+        }
+
+    }
+
+    private fun setData(detailsData: Details) = with(binding){
+
+        TODO() //тут сетим данные во вьюхи
+
     }
 
     override fun onDestroyView() {
